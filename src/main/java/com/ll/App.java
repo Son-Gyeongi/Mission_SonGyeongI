a8db983f5956bffd9dc43da62ba068cd82bef36f
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-06 10:06:30 +0900

    Quotation 1 step / end

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,9 +1,21 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class App {
 
-
     public void run() {
+        System.out.println("== 명언 앱 ==");
 
+        while (true) {
+            System.out.print("명령) ");
+
+            Scanner s = new Scanner(System.in);
+            String cmd = s.nextLine();
+
+            if (cmd.equals("종료")) {
+                break;
+            }
+        }
     }
 }
