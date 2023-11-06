285f2ce23d342413b86b7e5cf065520947b5d634
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-06 11:05:04 +0900

    quotation save, list refactoring

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -5,46 +5,56 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
+    Scanner s = new Scanner(System.in);
+    int lastQuotationId = 0; // 명언 번호
+    List<Quotation> quotations = new ArrayList<>(); // 명언 리스트
 
     public void run() {
         System.out.println("== 명언 앱 ==");
-        int lastQuotationId = 0; // 명언 번호
-        List<Quotation> quotations = new ArrayList<>();
 
         while (true) {
             System.out.print("명령) ");
 
-            Scanner s = new Scanner(System.in);
             String cmd = s.nextLine();
 
             if (cmd.equals("종료")) {
                 break;
             } else if (cmd.equals("등록")) {
-                System.out.print("명언 : ");
-                String content = s.nextLine();
-
-                System.out.print("작가 : ");
-                String authorName = s.nextLine();
-
-                lastQuotationId++;
-                int id = lastQuotationId;
-
-                // Quotation 객체 생성
-                Quotation quotation = new Quotation(id, authorName, content);
-                // 리스트에 저장
-                quotations.add(quotation);
-
-                System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                actionSave();
             } else if (cmd.equals("목록")) {
-                System.out.println("번호 / 작가 / 명언");
-                System.out.println("----------------------");
-
-                // quotations 리스트에서 모두 출력
-                for (int i = quotations.size() - 1; i >= 0; i--) {
-                    Quotation quotation = quotations.get(i);
-                    System.out.printf("%d / %s / %s\n", quotation.id, quotation.authorName, quotation.quotation);
-                }
+                actionList();
             }
+        }
+    }
+
+    // 명언 등록
+    void actionSave() {
+        System.out.print("명언 : ");
+        String content = s.nextLine();
+
+        System.out.print("작가 : ");
+        String authorName = s.nextLine();
+
+        lastQuotationId++;
+        int id = lastQuotationId;
+
+        // Quotation 객체 생성
+        Quotation quotation = new Quotation(id, authorName, content);
+        // 리스트에 저장
+        quotations.add(quotation);
+
+        System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+    }
+
+    // 명언 목록
+    void actionList() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+
+        // quotations 리스트에서 모두 출력
+        for (int i = quotations.size() - 1; i >= 0; i--) {
+            Quotation quotation = quotations.get(i);
+            System.out.printf("%d / %s / %s\n", quotation.id, quotation.authorName, quotation.quotation);
         }
     }
 }
