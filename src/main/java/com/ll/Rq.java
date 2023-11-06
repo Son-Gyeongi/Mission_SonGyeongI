0dbf9e9180d489d9dfa28ee86ad81107ba4946d4
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-06 16:51:05 +0900

    Rq List -> Map modify

--- src/main/java/com/ll/Rq.java
+++ src/main/java/com/ll/Rq.java
@@ -1,20 +1,15 @@
 package com.ll;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 // URL에서 queryString 분리 후 저장하는 클래스
 public class Rq {
     String cmd; // 입력받는 값
     String action; // 종료, 등록, 삭제, 수정
     String queryString; // id=2&archive=true
-    List<String> paramNames;
-    List<String> paramValues;
+    Map<String, String> paramMap;
 
     Rq(String cmd) {
-        paramNames = new ArrayList<>();
-        paramValues = new ArrayList<>();
-
         this.cmd = cmd;
 
         // 예시 :  cmd == 삭제?id=2&archive=true
@@ -28,11 +23,10 @@ public class Rq {
             String queryParamStr = queryStringBits[i];
             String[] queryParamStrBits = queryParamStr.split("=", 2);
 
-            String _paramName = queryParamStrBits[0];
+            String paramname = queryParamStrBits[0];
             String paramValue = queryParamStrBits[1];
 
-            paramNames.add(_paramName);
-            paramValues.add(paramValue);
+            paramMap.put(paramname, paramValue);
         }
     }
 
@@ -42,17 +36,16 @@ public class Rq {
 
     // "id"의 값 반환
     public int getParamAsInt(String paramName, int defaultValue) {
-        // indexOf() : 리스트에 맞는 값을 넣으면 인덱스값을 주고 없으면 -1을 반환한다.
-        int index = paramNames.indexOf(paramName); // paramName은 "id"
+        String paramValue = paramMap.get(paramName);
 
-        if (index == -1) return defaultValue;
+        if (paramValue != null) {
+            try {
+                return Integer.parseInt(paramValue);
+            } catch (NumberFormatException e) {
 
-        String paramValue = paramValues.get(index);
-
-        try {
-            return Integer.parseInt(paramValue);
-        } catch (NumberFormatException e) {
-            return defaultValue;
+            }
         }
+
+        return defaultValue;
     }
 }
